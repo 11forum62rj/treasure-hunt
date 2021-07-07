521efb7e9791b9789418e1a192a392b1beeafa7e
11forum62rj <73180905+11forum62rj@users.noreply.github.com>
2021-07-07 23:50:56 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 30,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 30,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 490,150)
+    fill("lightblue")
+    text("Hint: A set of statements that performs a task !!", 490,170)
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 930,250)
+    fill("lightblue")
+    text("Hint: it is an organized collection of structured information, or data !!", 930,270)
+     
+
+    
+
+}
